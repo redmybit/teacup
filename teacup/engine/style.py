6a16217a9a28fc09cce016f1f2fb3b42c3fde65c
redmybit <matthew.kosyanchuk@gmail.com>
2025-12-28 18:44:12 -0600

    Create style.py

---
+++ teacup/engine/style.py
@@ -0,0 +1,11 @@
+def get_style_template() -> dict:
+    """
+    Returns the default style templated used by all objects
+    """
+
+    return {
+        "color": (0, 0, 0, 255),
+        "background-color": (0, 0, 0, 0),
+        "font-size": 24,
+        "font-family": "Inter",
+    }
